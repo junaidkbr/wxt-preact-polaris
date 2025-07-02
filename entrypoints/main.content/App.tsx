4f282a9c5c7a051a3028f8f4869fc3e86e0fb5cc
Junaid Ahmed <junaidkbr@gmail.com>
2025-07-03 00:26:00 +0500

    Add s-text-field example

--- entrypoints/main.content/App.tsx
+++ entrypoints/main.content/App.tsx
@@ -1,26 +1,41 @@
-import { useState } from 'preact/hooks';
-
 export default function App() {
-  const [selectedValue, setSelectedValue] = useState('1');
-
   return (
     <>
-      <s-select
-        label="Presets"
-        labelAccessibilityVisibility="exclusive"
-        value={selectedValue}
-        onChange={(e) => {
-          const target = e.target as HTMLSelectElement;
+      <s-stack direction="block">
+        <s-select
+          label="s-select"
+          onInput={(e) => {
+            const target = e.target as HTMLSelectElement;
 
-          console.log('s-select:onChange');
-          console.log('s-select:target', target);
-          console.log('s-select:value', target.value);
+            console.log('s-select:onInput');
+            console.log('s-select:onInput:target', target);
+            console.log('s-select:onInput:value', target.value);
+          }}
+          onChange={(e) => {
+            const target = e.target as HTMLSelectElement;
+
+            console.log('s-select:onChange');
+            console.log('s-select:onChange:target', target);
+            console.log('s-select:onChange:value', target.value);
+          }}
+        >
+          <s-option value="1">Option 1</s-option>
+          <s-option value="2">Option 2</s-option>
+          <s-option value="3">Option 3</s-option>
+        </s-select>
+      </s-stack>
+
+      <s-text-field
+        label="Email"
+        name="email"
+        onInput={(e) => {
+          const target = e.target as HTMLInputElement;
+
+          console.log('s-text-field:onInput');
+          console.log('s-text-field:onInput:target', target);
+          console.log('s-text-field:onInput:value', target.value);
         }}
-      >
-        <s-option value="1">Option 1</s-option>
-        <s-option value="2">Option 2</s-option>
-        <s-option value="3">Option 3</s-option>
-      </s-select>
+      />
     </>
   );
 }
